634989b478bdcb001bb4c854c46baeba2af89b7e
Moritz Schönherr <moritz.schoenherr@gmail.com>
2017-12-31 14:31:55 +0100

    refactor: split out html page generation into pages.clj

--- src/rezipeas/handler.clj
+++ src/rezipeas/handler.clj
@@ -4,37 +4,11 @@
             [hiccup.core :refer :all]
             [hiccup.form :refer :all]
             [rezipeas.sql :refer :all]
+            [rezipeas.pages :refer :all]
             [ring.util.response :refer :all]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
 
 (db-setup)
-
-(defn new-recipe []
-  (html [:header [:h1 "Neues Rezept"]]
-        [:script "function newIngredient() {
-                  var ings = document.getElementsByClassName(\"ingredient\");
-                  var ing = ings[0];
-                  var newing = ing.cloneNode(true);
-                  var i;
-                  for (i = 0; i < 3; i++) {
-                      newing.childNodes[i].value = \"\";
-                  }
-                  var ingform = document.getElementById(\"ingredients\");
-                  ingform.appendChild(newing);
-                  }"]
-         [:main
-          [:form {:method "post" :action "/recipe/new"}
-           [:input {:type "text" :placeholder "Name des Rezepts" :name "name"}]
-           [:textarea {:placeholder "Einleitende Worte" :name "intro"}]
-           [:div#ingredients
-            [:button {:type "button" :onclick "newIngredient()"} "+ Zutat"]
-            [:div.ingredient
-             [:input {:type "number" :step "0.01" :placeholder "0.0" :name "quantity"}]
-             [:input {:type "text" :placeholder "Einheit" :name "unit"}]
-             [:input {:type "text" :placeholder "Zutat" :name "ingredient"}]]]
-           [:textarea {:placeholder "Rezept" :name "description"}]
-           [:textarea {:placeholder "Tip" :name "tip"}]
-           (submit-button "Fertig")]]))
 
 (defn save-new-recipe [req]
   (let [params (get req :params)
@@ -56,19 +30,6 @@
         (insert-rec-ing db {:rec_id rec_id, :ing_id ing_id, :quantity quantity, :unit unit})))
     (redirect "/recipe/all")))
 
-(defn show-all-recipies []
-  """Returns <ul> of links to all recipies."""
-  (let [recipies (get-recs db)]
-    (html
-     [:ul
-      (reduce (fn [st rec] (concat st "<li>" (get rec :name) "</li>")) "" recipies)])))
-
-(defn show-all-ingredients []
-  """Returns <ul> of all ingredients."""
-  (let [ingredients (get-ingredients db)]
-    (html
-     [:ul
-      (reduce (fn [st ing] (concat st "<li>" (get ing :name) "</li>")) "" ingredients)])))
       
 (defroutes app-routes
   (GET "/" [] (html [:h1 "Rezept des Tages"]))
---
+++ src/rezipeas/pages.clj
@@ -0,0 +1,47 @@
+(ns rezipeas.pages
+  (:require [hiccup.core :refer :all]
+            [hiccup.form :refer :all]
+            [rezipeas.sql :refer :all]))
+
+(defn show-all-recipies []
+  """Returns <ul> of links to all recipies."""
+  (let [recipies (get-recs db)]
+    (html
+     [:ul
+      (reduce (fn [st rec] (concat st "<li>" (get rec :name) "</li>")) "" recipies)])))
+
+(defn show-all-ingredients []
+  """Returns <ul> of all ingredients."""
+  (let [ingredients (get-ingredients db)]
+    (html
+     [:ul
+      (reduce (fn [st ing] (concat st "<li>" (get ing :name) "</li>")) "" ingredients)])))
+
+(defn new-recipe []
+  """Returns the page containing the form for new recipies."""
+  (html [:header [:h1 "Neues Rezept"]]
+        [:script "function newIngredient() {
+                  var ings = document.getElementsByClassName(\"ingredient\");
+                  var ing = ings[0];
+                  var newing = ing.cloneNode(true);
+                  var i;
+                  for (i = 0; i < 3; i++) {
+                      newing.childNodes[i].value = \"\";
+                  }
+                  var ingform = document.getElementById(\"ingredients\");
+                  ingform.appendChild(newing);
+                  }"]
+         [:main
+          [:form {:method "post" :action "/recipe/new"}
+           [:input {:type "text" :placeholder "Name des Rezepts" :name "name"}]
+           [:textarea {:placeholder "Einleitende Worte" :name "intro"}]
+           [:div#ingredients
+            [:button {:type "button" :onclick "newIngredient()"} "+ Zutat"]
+            [:div.ingredient
+             [:input {:type "number" :step "0.01" :placeholder "0.0" :name "quantity"}]
+             [:input {:type "text" :placeholder "Einheit" :name "unit"}]
+             [:input {:type "text" :placeholder "Zutat" :name "ingredient"}]]]
+           [:textarea {:placeholder "Rezept" :name "description"}]
+           [:textarea {:placeholder "Tip" :name "tip"}]
+           (submit-button "Fertig")]]))
+
