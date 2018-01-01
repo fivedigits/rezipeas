b1abb2b93e43b31a8572c9d048dffb06b20bd874
Moritz Schönherr <moritz.schoenherr@gmail.com>
2018-01-01 10:26:48 +0100

    feat: insert queries now skip entries violating unique constraint

--- resources/inserts.sql
+++ resources/inserts.sql
@@ -1,24 +1,24 @@
 -- :name insert-recipe :! :n
 -- :doc Insert a recipe into table recipies
-INSERT INTO recipies (name, intro, description, tip)
+INSERT OR IGNORE INTO recipies (name, intro, description, tip)
        VALUES (:name, :intro, :description, :tip);
 
 -- :name insert-tag :! :n
 -- :doc Insert a new tag into table tags
-INSERT INTO tags (name)
+INSERT OR IGNORE INTO tags (name)
        VALUES (:name);
 
 -- :name insert-ingredient :! :n
 -- :doc Inserts a new ingredients
-INSERT INTO ingredients (name)
+INSERT OR IGNORE INTO ingredients (name)
        VALUES (:name);
 
 -- :name insert-tag-rec :! :n
 -- :doc Inserts a tag - recipe tuple to tagrec table
-INSERT INTO tagrec (tag_id, rec_id)
+INSERT OR IGNORE INTO tagrec (tag_id, rec_id)
        VALUES (:tag_id, :rec_id);
 
 -- :name insert-rec-ing :! :n
 -- :doc Inserts a recipe - ingredient coupling
-INSERT INTO recing (rec_id, ing_id, unit, quantity)
+INSERT OR IGNORE INTO recing (rec_id, ing_id, unit, quantity)
        VALUES (:rec_id, :ing_id, :unit, :quantity);
